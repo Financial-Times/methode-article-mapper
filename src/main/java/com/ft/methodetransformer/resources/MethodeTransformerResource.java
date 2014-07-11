8148e20a2cf88e441ade4d28c179147c310f8b5d
Sarah Wells <sarah.wells@ft.com>
2014-07-11 16:03:08 +0100

    Return appropriate status codes on error

--- src/main/java/com/ft/methodetransformer/resources/MethodeTransformerResource.java
+++ src/main/java/com/ft/methodetransformer/resources/MethodeTransformerResource.java
@@ -1,7 +1,6 @@
 package com.ft.methodetransformer.resources;
 
 
-import java.util.Date;
 import java.util.UUID;
 
 import javax.ws.rs.GET;
@@ -16,6 +15,8 @@ import com.ft.api.jaxrs.errors.ClientError;
 import com.ft.api.util.transactionid.TransactionIdUtils;
 import com.ft.contentstoreapi.model.Content;
 import com.ft.methodeapi.model.EomFile;
+import com.ft.methodetransformer.methode.MethodeContentNotEligibleForPublishException;
+import com.ft.methodetransformer.methode.MethodeFileNotFoundException;
 import com.ft.methodetransformer.methode.MethodeFileService;
 import com.ft.methodetransformer.transformation.EomFileProcessorForContentStore;
 import com.yammer.metrics.annotation.Timed;
@@ -43,9 +44,35 @@ public class MethodeTransformerResource {
         if (uuid == null) {
             throw ClientError.status(400).context(uuid).error("uuid is required").exception();
         }
+        
+        try {
+        	EomFile eomFile = methodeFileService.fileByUuid(UUID.fromString(uuid), transactionId);
+    		return eomFileProcessorForContentStore.process(eomFile, transactionId);
+        } catch (MethodeFileNotFoundException e) {
+			throw ClientError.status(404)
+			.reason(ErrorMessage.METHODE_FILE_NOT_FOUND)
+			.exception(e);
+        } catch (MethodeContentNotEligibleForPublishException e) {
+        	throw ClientError.status(422)
+			.reason(ErrorMessage.METHODE_CONTENT_TYPE_NOT_SUPPORTED)
+			.exception(e);
+        }
 		
-		EomFile eomFile = methodeFileService.fileByUuid(UUID.fromString(uuid), transactionId);
-		return eomFileProcessorForContentStore.process(eomFile, transactionId);
-
     }
+	private enum ErrorMessage {
+		METHODE_FILE_NOT_FOUND("Article cannot be found in Methode"),
+		METHODE_CONTENT_TYPE_NOT_SUPPORTED("Unsupported article type - not a compound story");
+
+
+	    private final String text;
+
+	    ErrorMessage(String text) {
+	        this.text = text;
+	    }
+
+	    @Override
+	    public String toString() {
+	        return text;
+	    }
+	}
 }
