38af538e574566c2a77d89e1c4b72cae323d92ef
Sarah Wells <sarah.wells@ft.com>
2014-10-09 09:13:56 +0100

    Added some happy path tests

--- methode-article-transformer-service/src/test/java/com/ft/methodearticletransformer/methode/rest/RestMethodeFileServiceTest.java
+++ methode-article-transformer-service/src/test/java/com/ft/methodearticletransformer/methode/rest/RestMethodeFileServiceTest.java
@@ -1,8 +1,37 @@
 package com.ft.methodearticletransformer.methode.rest;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.reset;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.junit.Assert.assertNotNull;
+import io.dropwizard.client.JerseyClientConfiguration;
+import io.dropwizard.setup.Environment;
+import io.dropwizard.util.Duration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import com.codahale.metrics.MetricRegistry;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ft.jerseyhttpwrapper.ResilientClientBuilder;
 import com.ft.jerseyhttpwrapper.config.EndpointConfiguration;
+import com.ft.methodeapi.model.EomAssetType;
+import com.ft.methodeapi.model.EomFile;
 import com.ft.methodearticletransformer.configuration.AssetTypeRequestConfiguration;
 import com.ft.methodearticletransformer.configuration.MethodeApiEndpointConfiguration;
 import com.ft.methodearticletransformer.methode.MethodeApiUnavailableException;
@@ -11,28 +40,12 @@ import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.UrlMatchingStrategy;
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.sun.jersey.api.client.Client;
+
 import cucumber.api.java.After;
-import io.dropwizard.client.JerseyClientConfiguration;
-import io.dropwizard.setup.Environment;
-import io.dropwizard.util.Duration;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.UUID;
-
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.reset;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasProperty;
 
 public class RestMethodeFileServiceTest {
 
@@ -40,20 +53,32 @@ public class RestMethodeFileServiceTest {
 	private static final String ROOT = "/";
 	private static final UUID SAMPLE_UUID = UUID.randomUUID();
 	private static final String SAMPLE_TRANSACTION_ID = "tid_test_allieshaveleftus";
+	
+	private final String SYSTEM_ATTRIBUTES = "<props><productInfo><name>FTcom</name>\n" +
+            "<issueDate>20131219</issueDate>\n" +
+            "</productInfo>\n" +
+            "<workFolder>/FT/Companies</workFolder>\n" +
+            "<templateName>/SysConfig/Templates/FT/Base-Story.xml</templateName>\n" +
+            "<summary>t text text text text text text text text text text text text text text text text\n" +
+            " text text text text te...</summary><wordCount>417</wordCount></props>";
+
 
 	@ClassRule
-	public static WireMockClassRule wireMockRule = new WireMockClassRule(0); //will allocate a free port
+	public static WireMockClassRule methodeApiWireMockRule = new WireMockClassRule(0); //will allocate a free port
 
 	@Rule
-	public WireMockClassRule instanceRule = wireMockRule;
+	public WireMockClassRule instanceRule = methodeApiWireMockRule;
 
 	@Rule
 	public ExpectedException expectedException = ExpectedException.none();
+	
+	private ObjectMapper objectMapper;
 
 	private RestMethodeFileService restMethodeFileService;
 
 	@Before
 	public void setup() {
+	    objectMapper = new ObjectMapper();
 
 		JerseyClientConfiguration fastTimeOuts = new JerseyClientConfiguration();
 		fastTimeOuts.setConnectionTimeout(Duration.milliseconds(100));
@@ -79,15 +104,31 @@ public class RestMethodeFileServiceTest {
 
 		restMethodeFileService = new RestMethodeFileService(environment, client, methodeApiEndpointConfiguration);
 	}
+	
+	@Test
+	public void shouldSuccessfullyRetrieveEomFile() throws Exception {
+        stubFor(get(toFindEomFileUrl()).willReturn(anEomFileResponse()));
+        EomFile eomFile = restMethodeFileService.fileByUuid(SAMPLE_UUID, SAMPLE_TRANSACTION_ID);
+        assertNotNull(eomFile);
+        //TODO - check what got returned?
+	}
+	
+	@Test
+    public void shouldSuccessfullyRetrieveAssetTypes() throws Exception {
+        Set<String> assetIds = Sets.newHashSet("test1", "test2");
+        stubFor(post(toFindAssetTypesUrl()).willReturn(anAssetTypeResponseForAssetIds(assetIds)));
+        Map<String, EomAssetType> assetTypes = restMethodeFileService.assetTypes(assetIds, SAMPLE_TRANSACTION_ID);
+        assertNotNull(assetTypes);
+    }
 
 	@Test
 	public void shouldThrowMethodeFileNotFoundExceptionWhen404FromMethodeApi() {
-		stubFor(get(toFindEomFileUrl()).willReturn(aResponseWithCode(404)));
+	       stubFor(get(toFindEomFileUrl()).willReturn(aResponseWithCode(404)));
 
-		expectedException.expect(MethodeFileNotFoundException.class);
-		expectedException.expect(hasProperty("uuid", equalTo(SAMPLE_UUID)));
+	        expectedException.expect(MethodeFileNotFoundException.class);
+	        expectedException.expect(hasProperty("uuid", equalTo(SAMPLE_UUID)));
 
-		restMethodeFileService.fileByUuid(SAMPLE_UUID, SAMPLE_TRANSACTION_ID);
+	        restMethodeFileService.fileByUuid(SAMPLE_UUID, SAMPLE_TRANSACTION_ID);
 	}
 
 	@Test
@@ -102,10 +143,58 @@ public class RestMethodeFileServiceTest {
 	private UrlMatchingStrategy toFindEomFileUrl() {
 		return urlMatching("/eom-file/.*");
 	}
+	
+	private UrlMatchingStrategy toFindAssetTypesUrl() {
+        return urlMatching("/asset-type");
+    }
 
 	private ResponseDefinitionBuilder aResponseWithCode(int code) {
 		return aResponse().withStatus(code).withHeader("Content-type", "application/json");
 	}
+	
+	private ResponseDefinitionBuilder anEomFileResponse() throws Exception {
+	    return aResponse().withStatus(200).withHeader("Content-type", "application/json").withBody(eomFileBody());
+	}
+	
+	private String eomFileBody() throws Exception {
+	    final byte[] fileBytes = "blah, blah, blah".getBytes();
+	    EomFile eomFile = new EomFile("asdf", "someType", fileBytes, "some attributes", "WebRevise", SYSTEM_ATTRIBUTES);
+	    return objectMapper.writeValueAsString(eomFile);
+	}
+	
+	private ResponseDefinitionBuilder anAssetTypeResponseForAssetIds(Set assetIds) throws Exception {
+        return aResponse().withStatus(200).withHeader("Content-type", "application/json").withBody(assetTypeBody(assetIds));
+    }
+	
+	private String assetTypeBody(Set assetIds) throws Exception {
+	    Map<String, EomAssetType> expectedOutput = expectedOutputForGetAssetTypes(assetIds, 4);
+	    return objectMapper.writeValueAsString(expectedOutput);
+	}
+	
+    private Map<String, EomAssetType> expectedOutputForGetAssetTypes(Set<String> assetIds, int numberOfPartitions) {
+        List<List<String>> partitionedAssetIdentifiers = Lists.partition(Lists.newArrayList(assetIds), numberOfPartitions);
+        
+        Map<String, EomAssetType> expectedOutput = Maps.newHashMap();
+        
+        for (List<String> slice: partitionedAssetIdentifiers) {
+            Map<String, EomAssetType> expectedOutputForSlice = getExpectedAssetTypesForSlice(slice);
+            expectedOutput.putAll(expectedOutputForSlice);
+        }
+        
+        return expectedOutput;
+    }
+
+    private Map<String, EomAssetType> getExpectedAssetTypesForSlice(List<String> slice) {
+        Map<String, EomAssetType> expectedAssetTypesForSlice = Maps.newHashMap();
+        for(String uuid: slice) {
+            expectedAssetTypesForSlice.put(uuid, getEomAssetTypeForUuid(uuid));
+        }
+        return expectedAssetTypesForSlice;
+    }
+
+    private EomAssetType getEomAssetTypeForUuid(String uuid) {
+        return new EomAssetType.Builder().uuid(uuid).type("EOM:CompoundStory").build();
+    }
 
 	@After
 	public void cleanUp() {
