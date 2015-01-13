aa8f4bf3398bd43bdf70d9454f985253d15b4272
Mike Huniewicz <michal.huniewicz@ft.com>
2015-01-13 12:06:31 +0000

    Simplified XML configuration, significantly.

--- methode-article-transformer-service/src/main/java/com/ft/methodearticletransformer/transformation/PullQuoteInnerElementsRegistry.java
+++ methode-article-transformer-service/src/main/java/com/ft/methodearticletransformer/transformation/PullQuoteInnerElementsRegistry.java
@@ -1,67 +1,14 @@
 package com.ft.methodearticletransformer.transformation;
 
-import com.ft.bodyprocessing.xml.eventhandlers.RetainWithSpecificAttributesXMLEventHandler;
-import com.ft.bodyprocessing.xml.eventhandlers.RetainWithoutAttributesXMLEventHandler;
+import com.ft.bodyprocessing.xml.eventhandlers.RetainXMLEventHandler;
 import com.ft.bodyprocessing.xml.eventhandlers.StripXMLEventHandler;
-import com.ft.bodyprocessing.xml.eventhandlers.XMLEventHandler;
 import com.ft.bodyprocessing.xml.eventhandlers.XMLEventHandlerRegistry;
-
-import javax.xml.stream.events.Characters;
-import javax.xml.stream.events.EndElement;
-import javax.xml.stream.events.StartElement;
-import java.util.HashMap;
-import java.util.Map;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 public class PullQuoteInnerElementsRegistry extends XMLEventHandlerRegistry {
 
-	private Map<String, XMLEventHandler> startElementEventHandlersWrapper = new HashMap<>();
-	private Map<String, XMLEventHandler> endElementEventHandlersWrapper = new HashMap<>();
-
-	private MethodeBodyTransformationXMLEventHandlerRegistry handlerRegistry;
-
-	public PullQuoteInnerElementsRegistry(MethodeBodyTransformationXMLEventHandlerRegistry handlerRegistry) {
-		this.handlerRegistry = handlerRegistry;
-
-		this.registerStartAndEndElementEventHandlerWrapper(new StripXMLEventHandler(),
-				"p");
+	public PullQuoteInnerElementsRegistry() {
+		registerDefaultEventHandler(new StripXMLEventHandler());
+		registerCharactersEventHandler(new RetainXMLEventHandler());
 	}
 
-	@Override
-	public XMLEventHandler getEventHandler(StartElement event) {
-		XMLEventHandler eventHandler = this.startElementEventHandlersWrapper.get(event.asStartElement().getName().getLocalPart().toLowerCase());
-		if (eventHandler == null) {
-			eventHandler = handlerRegistry.getEventHandler(event.asStartElement());
-		}
-		return eventHandler;
-	}
-
-	@Override
-	public XMLEventHandler getEventHandler(EndElement event) {
-		XMLEventHandler eventHandler = this.endElementEventHandlersWrapper.get(event.asEndElement().getName().getLocalPart().toLowerCase());
-		if (eventHandler == null) {
-			eventHandler = handlerRegistry.getEventHandler(event.asEndElement());
-		}
-		return eventHandler;
-	}
-
-	@Override
-	public XMLEventHandler getEventHandler(Characters event) {
-		return  handlerRegistry.getEventHandler(event);
-	}
-
-	private void registerStartAndEndElementEventHandlerWrapper(XMLEventHandler
-																	   startEndElementEventHandler, String... names) {
-		checkNotNull(startEndElementEventHandler, "startEndElementEventHandler cannot be null");
-		checkNotNull(names, "names cannot be null");
-		if (names.length == 0) {
-			throw new IllegalArgumentException("names cannot be empty");
-		}
-
-		for(String name: names) {
-			this.startElementEventHandlersWrapper.put(name.toLowerCase(), startEndElementEventHandler);
-			this.endElementEventHandlersWrapper.put(name.toLowerCase(), startEndElementEventHandler);
-		}
-	}
 }
