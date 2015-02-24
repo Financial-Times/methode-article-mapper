fd078f6fbefa1c96963c4d8e1f49b76a2a7b6771
Scott Ace Newton <scott.newton@ft.com>
2015-02-24 17:00:02 +0000

    Merge branch 'AddMissingVideoAttributes' of ssh://git.svc.ft.com:7999/cp/methode-article-transformer into bugfix/UPP-271-remove-all-elements-with-strikeout
    Conflicts:
    	methode-article-transformer-service/src/test/java/com/ft/methodearticletransformer/transformation/BodyProcessingFieldTransformerFactoryTest.java

--- methode-article-transformer-service/src/main/java/com/ft/methodearticletransformer/transformation/StrikeoutXMLEventHandler.java
+++ methode-article-transformer-service/src/main/java/com/ft/methodearticletransformer/transformation/StrikeoutXMLEventHandler.java
@@ -1,4 +1,114 @@
 package com.ft.methodearticletransformer.transformation;
 
-public class StrikeoutXMLEventHandler {
+import com.ft.bodyprocessing.BodyProcessingContext;
+import com.ft.bodyprocessing.BodyProcessingException;
+import com.ft.bodyprocessing.writer.BodyWriter;
+import com.ft.bodyprocessing.xml.eventhandlers.BaseXMLEventHandler;
+import com.ft.bodyprocessing.xml.eventhandlers.XMLEventHandler;
+
+import javax.management.Attribute;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.*;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class StrikeoutXMLEventHandler extends BaseXMLEventHandler {
+
+    private final XMLEventHandler fallbackEventHandler;
+    private final StartElementMatcher matcher;
+    private final String excludeFromStrikeoutElement;
+
+    private static final String PARAGRAPH_TAG = "p";
+
+    protected StrikeoutXMLEventHandler(final XMLEventHandler fallbackEventHandler, final StartElementMatcher matcher, String excludeFromStrikeoutElement) {
+        checkArgument(fallbackEventHandler != null, "fallbackEventHandler cannot be null");
+        checkArgument(matcher != null, "matcher cannot be null");
+        this.fallbackEventHandler = fallbackEventHandler;
+        this.matcher = matcher;
+        this.excludeFromStrikeoutElement = excludeFromStrikeoutElement;
+    }
+
+    @Override
+    public void handleStartElementEvent(final StartElement event, final XMLEventReader xmlEventReader, final BodyWriter eventWriter,
+                                        final BodyProcessingContext bodyProcessingContext) throws XMLStreamException {
+        if(!matcher.matches(event)) {
+            fallbackEventHandler.handleStartElementEvent(event, xmlEventReader, eventWriter, bodyProcessingContext);
+            return;
+        }
+        final String nameToMatch = event.getName().getLocalPart();
+        if(!(PARAGRAPH_TAG.equals(nameToMatch))) {
+            skipUntilMatchingEndTag(nameToMatch, xmlEventReader);
+            return;
+        }
+        List<XMLEvent> eventList = makeListOfXMLEventsAndSkip(event, xmlEventReader, event.getName().toString(), excludeFromStrikeoutElement);
+        if (eventList != null) {
+            try {
+                writeSavedEventList(eventList, eventWriter);
+            } catch (IOException e) {
+                throw new TransformationException(e);
+            }
+        }
+    }
+
+    @Override // Only called where the start tag used the fallback event handler
+    public void handleEndElementEvent(final EndElement event, final XMLEventReader xmlEventReader, final BodyWriter eventWriter) throws XMLStreamException {
+        fallbackEventHandler.handleEndElementEvent(event, xmlEventReader, eventWriter);
+    }
+
+    private List<XMLEvent> makeListOfXMLEventsAndSkip(StartElement event, XMLEventReader xmlEventReader, String primaryElementName, String secondaryElementName) throws XMLStreamException {
+        List<XMLEvent> xmlEventList = new ArrayList<>();
+        boolean containsSecondaryElement = false;
+        int primaryOpenElementNameCount = 1;
+        xmlEventList.add(event);
+        while(xmlEventReader.hasNext()) {
+            XMLEvent nextEvent = xmlEventReader.nextEvent();
+            if(nextEvent.isStartElement()) {
+                StartElement newStartElement = nextEvent.asStartElement();
+                xmlEventList.add(newStartElement);
+                if ((primaryElementName).equals(newStartElement.getName().getLocalPart())) {
+                    primaryOpenElementNameCount++;
+                }
+                if ((secondaryElementName).equals(newStartElement.getName().getLocalPart())) {
+                    containsSecondaryElement = true;
+                }
+            }
+            if(nextEvent.isCharacters()) {
+                Characters chars = nextEvent.asCharacters();
+                xmlEventList.add(chars);
+            }
+            if(nextEvent.isEndElement()) {
+                EndElement newEndElement = nextEvent.asEndElement();
+                xmlEventList.add(newEndElement);
+                if((primaryElementName).equals(newEndElement.getName().getLocalPart())) {
+                    if(primaryOpenElementNameCount == 1) {
+                        if(containsSecondaryElement) {
+                            return xmlEventList;
+                        }
+                        return null;
+                    }
+                    primaryOpenElementNameCount--;
+                }
+            }
+        }
+        throw new BodyProcessingException("Reached end without encountering closing primary tag: " + primaryElementName);
+    }
+
+    private void writeSavedEventList(List<XMLEvent> eventList, BodyWriter eventWriter) throws XMLStreamException, IOException{
+        Writer writer = new StringWriter();
+        for (XMLEvent event : eventList) {
+            event.writeAsEncodedUnicode(writer);
+        }
+        eventWriter.writeRaw(writer.toString());
+    }
+
+
 }
+
--- methode-article-transformer-service/src/test/java/com/ft/methodearticletransformer/transformation/StrikeoutXMLEventHandlerTest.java
+++ methode-article-transformer-service/src/test/java/com/ft/methodearticletransformer/transformation/StrikeoutXMLEventHandlerTest.java
@@ -1,4 +1,120 @@
 package com.ft.methodearticletransformer.transformation;
 
-public class StrikeoutXMLEventHandlerTest {
+import com.ft.bodyprocessing.BodyProcessingContext;
+import com.ft.bodyprocessing.writer.BodyWriter;
+import com.ft.bodyprocessing.xml.eventhandlers.BaseXMLEventHandler;
+import com.ft.bodyprocessing.xml.eventhandlers.XMLEventHandler;
+import com.google.common.collect.ImmutableMap;
+import org.codehaus.stax2.XMLEventReader2;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+
+import java.io.StringWriter;
+import java.util.Collections;
+
+import static com.ft.methodearticletransformer.transformation.StrikeoutEventHandlerRegistry.attributeNameMatcher;
+import static org.mockito.Mockito.*;
+
+
+@RunWith(value=MockitoJUnitRunner.class)
+public class StrikeoutXMLEventHandlerTest extends BaseXMLEventHandlerTest {
+    private BaseXMLEventHandler eventHandler;
+
+    @Mock private XMLEventReader2 mockXmlEventReader;
+    @Mock private BodyWriter eventWriter;
+    @Mock private StringWriter mockStringWriter;
+    @Mock private BodyProcessingContext mockBodyProcessingContext;
+    @Mock private XMLEventHandler fallbackEventHandler;
+    @Mock private FieldTransformer mockTransformer;
+
+    @Before
+    public void setup() throws Exception {
+        eventHandler = new StrikeoutXMLEventHandler(fallbackEventHandler, attributeNameMatcher("channel"), "iframe");
+    }
+
+    @Test
+    public void shouldUseFallbackHandlerForStartTagIfAttributesDoNotMatch() throws Exception {
+        ImmutableMap<String,String> attributesMap = ImmutableMap.of("title", "!");
+        StartElement startElement = getStartElementWithAttributes("p", attributesMap);
+        eventHandler.handleStartElementEvent(startElement, mockXmlEventReader, eventWriter, mockBodyProcessingContext);
+        verify(fallbackEventHandler).handleStartElementEvent(startElement, mockXmlEventReader, eventWriter, mockBodyProcessingContext);
+    }
+
+    @Test
+    public void shouldUseFallbackHandlerForStartTagIfNoAttributes() throws Exception {
+        StartElement startElement = getStartElement("p");
+        eventHandler.handleStartElementEvent(startElement, mockXmlEventReader, eventWriter, mockBodyProcessingContext);
+        verify(fallbackEventHandler).handleStartElementEvent(startElement, mockXmlEventReader, eventWriter, mockBodyProcessingContext);
+    }
+
+    @Test
+    public void shouldAlwaysUseFallBackHandlerForEndTag() throws Exception {
+        EndElement endElement = getEndElement("p");
+        eventHandler.handleEndElementEvent(endElement, mockXmlEventReader, eventWriter);
+        verify(fallbackEventHandler).handleEndElementEvent(endElement, mockXmlEventReader, eventWriter);
+    }
+
+    @Test
+    public void shouldRemoveStartTagAndContentsUpToMatchingEndTagIfStrikeoutAttributePresent() throws Exception {
+        ImmutableMap<String,String> attributesMap = ImmutableMap.of("channel", "!");
+        StartElement startElement = getStartElementWithAttributes("span", attributesMap);
+        when(mockXmlEventReader.hasNext()).thenReturn(true, true, true, true, true, true, true);
+        //"<span channel="!">Some text in <i>italics</i> and some not</span>"
+        when(mockXmlEventReader.nextEvent()).thenReturn(getCharacters("Some text in "),
+                getStartElement("i"), getCharacters("italics"), getEndElement("i"),
+                getCharacters(" and some not"), getEndElement("span"));
+        when(mockXmlEventReader.peek()).thenReturn(getCharacters("Some text in "),
+                getStartElement("i"), getCharacters("italics"), getEndElement("i"),
+                getCharacters(" and some not"), getEndElement("span"));
+        eventHandler.handleStartElementEvent(startElement, mockXmlEventReader, eventWriter, mockBodyProcessingContext);
+        verifyZeroInteractions(eventWriter);
+    }
+
+    @Test
+    public void shouldRemoveStartTagAndContentsUpToMatchingEndTagIfPTagWithChannelAttibuteButNoNestedIFrameElementPresent() throws Exception {
+        ImmutableMap<String, String> attributesMap = ImmutableMap.of("channel", "!");
+        StartElement startElement = getStartElementWithAttributes("p", attributesMap);
+        when(mockXmlEventReader.hasNext()).thenReturn(true, true, true);
+        //"<p channel="!">Look here for content</p>"
+        when(mockXmlEventReader.nextEvent()).thenReturn(getCharacters("Look here for content"), getEndElement("p"));
+        when(mockXmlEventReader.peek()).thenReturn(getCharacters("Look here for content"), getEndElement("p"));
+        eventHandler.handleStartElementEvent(startElement, mockXmlEventReader, eventWriter, mockBodyProcessingContext);
+        verifyZeroInteractions(eventWriter);
+    }
+
+    @Test
+    public void shouldRemoveStartTagAndContentsUpToMatchingEndTagIfChannelAttibuteAndNestedIFrameElementPresentButHasNoOuterPTag() throws Exception {
+        ImmutableMap<String, String> attributesMap = ImmutableMap.of("channel", "!");
+        StartElement startElement = getStartElementWithAttributes("span", attributesMap);
+        when(mockXmlEventReader.hasNext()).thenReturn(true, true, true);
+        //"<span channel="!">Look here <iframe></iframe> for content</span>"
+        when(mockXmlEventReader.nextEvent()).thenReturn(getCharacters("Look here "), getStartElement("iframe"), getEndElement("iframe"),getCharacters(" for content"), getEndElement("span"));
+        when(mockXmlEventReader.peek()).thenReturn(getCharacters("Look here "), getStartElement("iframe"), getEndElement("iframe"),getCharacters(" for content"), getEndElement("span"));
+        eventHandler.handleStartElementEvent(startElement, mockXmlEventReader, eventWriter, mockBodyProcessingContext);
+        verifyZeroInteractions(eventWriter);
+    }
+
+/*    @Test
+    public void shouldRetainStartTagAndContentsUpToMatchingEndTagIfPTagWithChannelAttibuteAndNestedIFrameElementPresent() throws Exception {
+        ImmutableMap<String, String> attributesMap = ImmutableMap.of("channel", "!");
+        StartElement pElement = getStartElementWithAttributes("p", attributesMap);
+        StartElement iFrameElement = getStartElement("iframe");
+        when(mockXmlEventReader.hasNext()).thenReturn(true, true, true, true, true);
+        //"<p channel="!">Look here <iframe></iframe> for content</p>"
+        when(mockXmlEventReader.nextEvent()).thenReturn(getCharacters("Look here "), (getStartElementWithAttributes(iFrameElement.getName().getLocalPart(), Collections.singletonMap("src", "www.youtube.com"))),
+                getEndElement("iframe"), getCharacters(" for content"), getEndElement("p"));
+        when(mockXmlEventReader.peek()).thenReturn(getCharacters("Look here "), (getStartElementWithAttributes(iFrameElement.getName().getLocalPart(), Collections.singletonMap("src", "www.youtube.com"))),
+                getEndElement("iframe"), getCharacters(" for content"), getEndElement("p"));
+        eventHandler.handleStartElementEvent(pElement, mockXmlEventReader, eventWriter, mockBodyProcessingContext);
+        verify(eventWriter, atLeastOnce()).writeRaw("<p channel=\"!\">Look here <iframe src=\"www.youtube.com\"></iframe> for content</p>");
+        //verify(eventWriter).writeStartTag(pElement.getName().getLocalPart(), attributesMap);
+    }*/
+
+
 }
