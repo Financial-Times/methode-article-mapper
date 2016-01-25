627c5d11939c2eccb15e5f218942a96a19714471
Julia Fernee <julia.fernee@ft.com>
2016-01-25 17:15:34 +0000

    UPP-1081 create POST handler for published content

--- methode-article-transformer-service/src/main/java/com/ft/methodearticletransformer/resources/MethodeArticleTransformerResource.java
+++ methode-article-transformer-service/src/main/java/com/ft/methodearticletransformer/resources/MethodeArticleTransformerResource.java
@@ -18,6 +18,7 @@ import com.ft.methodearticletransformer.model.EomFile;
 import com.ft.methodearticletransformer.transformation.EomFileProcessorForContentStore;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -120,7 +121,67 @@ public class MethodeArticleTransformerResource {
         return context;
     }
 
-    public enum ErrorMessage {
+	@POST
+	@Timed
+	@Path("/{uuidString}")
+	@Produces(MediaType.APPLICATION_JSON + CHARSET_UTF_8)
+	public final Content getByUuid(@PathParam("uuidString") String uuidString, EomFile eomFile, @Context HttpHeaders httpHeaders) {
+
+		String transactionId = TransactionIdUtils.getTransactionIdOrDie(httpHeaders, uuidString, "Publish request");
+		if (uuidString == null) {
+			throw ClientError.status(400).context(uuidString).reason(ErrorMessage.UUID_REQUIRED).exception();
+		}
+
+		UUID uuid;
+		try {
+			uuid = UUID.fromString(uuidString);
+		} catch (IllegalArgumentException iae) {
+			throw ClientError.status(400)
+					.reason(ErrorMessage.INVALID_UUID)
+					.exception(iae);
+		}
+		try {
+			return eomFileProcessorForContentStore.process(eomFile, transactionId);
+
+		} catch (SourceApiUnavailableException e) {
+			throw ServerError.status(503)
+					.reason(ErrorMessage.METHODE_API_UNAVAILABLE)
+					.exception(e);
+		} catch (ResourceNotFoundException e) {
+			throw ClientError.status(404)
+					.reason(ErrorMessage.METHODE_FILE_NOT_FOUND)
+					.exception(e);
+		} catch (MethodeMarkedDeletedException e) {
+			throw ClientError.status(404)
+					.context(uuid)
+					.reason(ErrorMessage.METHODE_FILE_NOT_FOUND)
+					.exception(e);
+		} catch (NotWebChannelException e) {
+			throw ClientError.status(404)
+					.reason(ErrorMessage.NOT_WEB_CHANNEL)
+					.exception(e);
+		} catch (MethodeMissingFieldException e) {
+			throw ClientError.status(404)
+					.error(String.format(ErrorMessage.METHODE_FIELD_MISSING.toString(), e.getFieldName()))
+					.exception(e);
+		} catch (MethodeMissingBodyException e) {
+			throw ClientError.status(418)
+					.error(e.getMessage())
+					.exception(e);
+		} catch (MethodeContentNotEligibleForPublishException e) {
+			throw ClientError.status(404)
+					.context(uuid)
+					.error(e.getMessage())
+					.exception(e);
+		} catch (DocumentStoreApiUnavailableException e) {
+			throw ServerError.status(503)
+					.reason(ErrorMessage.DOCUMENT_STORE_API_UNAVAILABLE)
+					.exception(e);
+		}
+
+	}
+
+	public enum ErrorMessage {
 		METHODE_FILE_NOT_FOUND("Article cannot be found in Methode"),
 		UUID_REQUIRED("No UUID was passed"),
 		INVALID_UUID("The UUID passed was invalid"),
