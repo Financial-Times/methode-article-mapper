570f3098d636b75f3bb82b13dcf3f842066e3ab4
Julia Fernee <julia.fernee@ft.com>
2016-02-01 13:44:57 +0000

    UPP-1081 Unit tests for POST endpoint and article preview transformation

---
+++ methode-article-transformer-service/src/test/java/com/ft/methodearticletransformer/resources/PostContentToTransformResourceForPreviewUnhappyPathsTest.java
@@ -0,0 +1,93 @@
+package com.ft.methodearticletransformer.resources;
+
+import com.ft.api.jaxrs.errors.ErrorEntity;
+import com.ft.api.jaxrs.errors.WebApplicationClientException;
+import com.ft.api.util.transactionid.TransactionIdUtils;
+import com.ft.methodearticletransformer.methode.UnsupportedTypeException;
+import com.ft.methodearticletransformer.model.EomFile;
+import com.ft.methodearticletransformer.transformation.EomFileProcessorForContentStore;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.HttpHeaders;
+import java.util.Arrays;
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests that application exceptions that occur during transformation of article preview translate into correct HTTP codes by the POST endpoint.
+ *
+ * Created by julia.fernee on 28/01/2016.
+ */
+public class PostContentToTransformResourceForPreviewUnhappyPathsTest {
+
+    private static final boolean IS_PREVIEW_TRUE = true;
+    private static final String TRANSACTION_ID = "tid_test";
+    private static final String INVALID_TYPE = "NOT_COMPOUND_STORY";
+
+    private EomFileProcessorForContentStore eomFileProcessorForContentStore = mock(EomFileProcessorForContentStore.class);
+    private HttpHeaders httpHeaders = mock(HttpHeaders.class);
+    private EomFile eomFile = mock(EomFile.class);
+
+    /*Class under test*/
+    private PostContentToTransformResource postContentToTransformResource = new PostContentToTransformResource(eomFileProcessorForContentStore);;
+
+    @Before
+    public void preconditions() {
+        when(httpHeaders.getRequestHeader(TransactionIdUtils.TRANSACTION_ID_HEADER)).thenReturn(Arrays.asList(TRANSACTION_ID));
+    }
+
+    @Test
+    public void shouldThrow400ExceptionWhenNoUuidPassed() {
+        try {
+            postContentToTransformResource.doTransform(null, IS_PREVIEW_TRUE, eomFile, httpHeaders);
+            fail("No exception was thrown, but expected one.");
+        } catch (WebApplicationClientException wace) {
+            assertThat(((ErrorEntity)wace.getResponse().getEntity()).getMessage(),
+                    equalTo(ErrorMessage.UUID_REQUIRED.toString()));
+            assertThat(wace.getResponse().getStatus(), equalTo(HttpStatus.SC_BAD_REQUEST));
+        } catch (Throwable throwable) {
+            fail(String.format("The thrown exception was not of expected type. It was [%s] instead.",
+                    throwable.getClass().getCanonicalName()));
+        }
+    }
+
+    @Test
+    public void shouldThrow400ExceptionWhenInvalidUuidPassed() {
+        try {
+            postContentToTransformResource.doTransform("invalid_transaction_id", IS_PREVIEW_TRUE, eomFile, httpHeaders);
+            fail("No exception was thrown, but expected one.");
+        } catch (WebApplicationClientException wace) {
+            assertThat(((ErrorEntity)wace.getResponse().getEntity()).getMessage(),
+                    equalTo(ErrorMessage.INVALID_UUID.toString()));
+            assertThat(wace.getResponse().getStatus(), equalTo(HttpStatus.SC_BAD_REQUEST));
+        } catch (Throwable throwable) {
+            fail(String.format("The thrown exception was not of expected type. It was [%s] instead.",
+                    throwable.getClass().getCanonicalName()));
+        }
+    }
+
+    @Test
+    public void shouldThrow404ExceptionWhenPrviewNotEligibleForPublishing() {
+        UUID randomUuid = UUID.randomUUID();
+        when(eomFileProcessorForContentStore.processPreview(eomFile, TRANSACTION_ID)).
+                thenThrow(new UnsupportedTypeException(randomUuid, INVALID_TYPE));
+        try {
+            postContentToTransformResource.doTransform(randomUuid.toString(), IS_PREVIEW_TRUE, eomFile, httpHeaders);
+            fail("No exception was thrown, but expected one.");
+        } catch (WebApplicationClientException wace) {
+            assertThat(((ErrorEntity)wace.getResponse().getEntity()).getMessage(),
+                    equalTo(String.format("[%s] not an EOM::CompoundStory.", INVALID_TYPE)));
+            assertThat(wace.getResponse().getStatus(), equalTo(HttpStatus.SC_NOT_FOUND));
+        } catch (Throwable throwable) {
+            fail(String.format("The thrown exception was not of expected type. It was [%s] instead.",
+                    throwable.getClass().getCanonicalName()));
+        }
+    }
+}
