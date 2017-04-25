23f7543390a38bf7c1aed318c0f179ff7c1a32af
Iulian Popa <iulian.popa@ft.com>
2017-04-25 08:20:09 +0300

    Removed unused import.

--- src/test/java/com/ft/methodearticlemapper/transformation/BaseXMLEventHandlerTest.java
+++ src/test/java/com/ft/methodearticlemapper/transformation/BaseXMLEventHandlerTest.java
@@ -1,7 +1,6 @@
 package com.ft.methodearticlemapper.transformation;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.xml.namespace.QName;
