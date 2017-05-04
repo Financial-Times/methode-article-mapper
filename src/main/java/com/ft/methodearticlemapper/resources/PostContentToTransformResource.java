4908276eb271d6ac8025efd488318a285c2051e2
Bogdan Guranda <bogdan.guranda@ft.com>
2017-05-04 16:54:27 +0300

    Better UUID validation methode + clean-up.

--- src/main/java/com/ft/methodearticlemapper/resources/PostContentToTransformResource.java
+++ src/main/java/com/ft/methodearticlemapper/resources/PostContentToTransformResource.java
@@ -4,7 +4,6 @@ import com.codahale.metrics.annotation.Timed;
 import com.ft.api.jaxrs.errors.ClientError;
 import com.ft.api.util.transactionid.TransactionIdUtils;
 import com.ft.content.model.Content;
-import com.ft.methodearticlemapper.exception.MethodeContentInvalidException;
 import com.ft.methodearticlemapper.exception.MethodeContentNotEligibleForPublishException;
 import com.ft.methodearticlemapper.exception.MethodeMarkedDeletedException;
 import com.ft.methodearticlemapper.exception.MethodeMissingBodyException;
@@ -17,7 +16,6 @@ import com.ft.methodearticlemapper.transformation.EomFileProcessor;
 import java.util.Date;
 import java.util.UUID;
 
-import java.util.regex.Pattern;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -30,8 +28,6 @@ import javax.ws.rs.core.MediaType;
 public class PostContentToTransformResource {
 
     private static final String CHARSET_UTF_8 = ";charset=utf-8";
-
-    private final Pattern uuidPattern = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f‌​]{4}-[0-9a-f]{12}$");
 
     private final EomFileProcessor eomFileProcessor;
 
@@ -48,7 +44,7 @@ public class PostContentToTransformResource {
 	public final Content map(EomFile eomFile, @QueryParam("preview") boolean preview, @Context HttpHeaders httpHeaders) {
 		final String transactionId = TransactionIdUtils.getTransactionIdOrDie(httpHeaders);
 		final String uuid = eomFile.getUuid();
-		validateUuid(uuid, eomFile);
+		validateUuid(uuid);
 		return processRequest(uuid, preview, eomFile, transactionId);
 	}
 
@@ -85,21 +81,14 @@ public class PostContentToTransformResource {
         }
     }
 
-    private void validateUuid(String uuid, EomFile eomFile) {
+    private void validateUuid(String uuid) {
         if (uuid == null) {
             throw ClientError.status(400).context(null).reason(ErrorMessage.UUID_REQUIRED).exception();
         }
         try {
-            if (!uuidPattern.matcher(uuid).matches()) {
-                throw new IllegalArgumentException();
-            }
-            UUID resourceId = UUID.fromString(uuid);
-
-            if (!uuid.equals(eomFile.getUuid())) {
-                String errorMessage = String.format(ErrorMessage.CONFLICTING_UUID.toString(), uuid, eomFile.getUuid());
-                throw ClientError.status(409)
-                        .error(errorMessage)
-                        .exception(new MethodeContentInvalidException(resourceId, errorMessage));
+            UUID modelUUID = UUID.fromString(uuid);
+            if (!uuid.equals(modelUUID.toString())) {
+                throw new IllegalArgumentException("Invalid UUID: " + uuid);
             }
         } catch (IllegalArgumentException iae) {
             throw ClientError.status(400)
@@ -109,14 +98,11 @@ public class PostContentToTransformResource {
     }
 
     enum ErrorMessage {
-        METHODE_FILE_NOT_FOUND("Article marked as deleted"),
         UUID_REQUIRED("No UUID was passed"),
         INVALID_UUID("The UUID passed was invalid"),
-        METHODE_CONTENT_TYPE_NOT_SUPPORTED("Invalid request - resource not an article"),
+        METHODE_FILE_NOT_FOUND("Article marked as deleted"),
         NOT_WEB_CHANNEL("This is not a web channel story"),
-        METHODE_FIELD_MISSING("Required methode field [%s] is missing"),
-        DOCUMENT_STORE_API_UNAVAILABLE("Document store API was unavailable"),
-        CONFLICTING_UUID("UUID in the url [%s] and uuid in the payload [%s] are not the same");
+        METHODE_FIELD_MISSING("Required methode field [%s] is missing");
 
         private final String text;
 
