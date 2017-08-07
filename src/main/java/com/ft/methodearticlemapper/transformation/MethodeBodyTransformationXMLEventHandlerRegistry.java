4248022dd8add427717f7b6e810c7f482dbbbfc6
Hunor Kovacs <hunor.kovacs@ft.com>
2017-08-07 14:12:28 +0300

    Removing <recommended> tags explicitly from article bodies. Kitchensink test updated as well.

--- src/main/java/com/ft/methodearticlemapper/transformation/MethodeBodyTransformationXMLEventHandlerRegistry.java
+++ src/main/java/com/ft/methodearticlemapper/transformation/MethodeBodyTransformationXMLEventHandlerRegistry.java
@@ -70,7 +70,8 @@ public class MethodeBodyTransformationXMLEventHandlerRegistry extends XMLEventHa
                 "story", "strap", "videoObject", "web-alt-picture", "web-background-news",
                 "web-background-news-header", "web-background-news-text",
                 "web-picture", "web-pull-quote-source", "web-pull-quote-text",
-                "web-skybox-picture", "web-subhead", "web-thumbnail", "xref", "xrefs"
+                "web-skybox-picture", "web-subhead", "web-thumbnail", "xref", "xrefs",
+                "recommended"
         );
 
         registerStartAndEndElementEventHandler(new SimpleTransformTagXmlEventHandler("h3", "class", "ft-subhead"), "subhead");
