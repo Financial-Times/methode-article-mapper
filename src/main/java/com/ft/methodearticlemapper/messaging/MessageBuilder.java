5d299cc661be26507a48bc7faea7fd8f70802f20
Mihai Moisa <mihai.moisa@ft.com>
2018-08-29 15:02:55 +0300

    Set ContentType Header for kafka messages

--- src/main/java/com/ft/methodearticlemapper/messaging/MessageBuilder.java
+++ src/main/java/com/ft/methodearticlemapper/messaging/MessageBuilder.java
@@ -1,13 +1,15 @@
 package com.ft.methodearticlemapper.messaging;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ft.content.model.Content;
 import com.ft.messagequeueproducer.model.KeyedMessage;
 import com.ft.messaging.standards.message.v1.Message;
 import com.ft.messaging.standards.message.v1.SystemId;
 import com.ft.methodearticlemapper.exception.MethodeArticleMapperException;
+import com.ft.methodearticlemapper.exception.MissingMappingForContentTypeException;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
@@ -26,53 +28,56 @@ public class MessageBuilder {
     private static final String CMS_CONTENT_PUBLISHED = "cms-content-published";
     private static final DateTimeFormatter RFC3339_FMT =
             DateTimeFormatter.ISO_OFFSET_DATE_TIME.withResolverStyle(ResolverStyle.STRICT);
-    private static final String TYPE_BASE_URL = "http://www.ft.com/ontology/content/";
 
     private final UriBuilder contentUriBuilder;
     private final SystemId systemId;
     private final ObjectMapper objectMapper;
+    private final Map<String, String> contentTypeMappings;
 
-    public MessageBuilder(UriBuilder contentUriBuilder, String systemId, ObjectMapper objectMapper) {
+    public MessageBuilder(UriBuilder contentUriBuilder, String systemId, ObjectMapper objectMapper, Map<String, String> contentTypeMappings) {
         this.contentUriBuilder = contentUriBuilder;
         this.systemId = SystemId.systemIdFromCode(systemId);
         this.objectMapper = objectMapper;
+        this.contentTypeMappings = contentTypeMappings;
     }
 
-    Message buildMessage(Content content, Map<String,String> headers) {
+    Message buildMessage(Content content, Map<String, String> headers) {
         MessageBody msgBody = new MessageBody(
                 content,
                 contentUriBuilder.build(content.getUuid()).toString(),
-                RFC3339_FMT.format(OffsetDateTime.ofInstant(content.getLastModified().toInstant(), UTC)),
-                TYPE_BASE_URL + content.getType()
+                RFC3339_FMT.format(OffsetDateTime.ofInstant(content.getLastModified().toInstant(), UTC))
         );
-
-        return buildMessage(content.getUuid(), content.getPublishReference(), headers, msgBody);
+        return buildMessage(content.getUuid(), content.getPublishReference(), headers, msgBody, content.getType());
     }
 
-    Message buildMessageForDeletedMethodeContent(String uuid, String publishReference, Date lastModified, String contentType, Map<String,String> headers) {
+    Message buildMessageForDeletedMethodeContent(String uuid, String publishReference, Date lastModified, String contentType, Map<String, String> headers) {
         MessageBody msgBody = new MessageBody(
                 null,
                 contentUriBuilder.build(uuid).toString(),
-                RFC3339_FMT.format(OffsetDateTime.ofInstant(lastModified.toInstant(), UTC)),
-                TYPE_BASE_URL + contentType
+                RFC3339_FMT.format(OffsetDateTime.ofInstant(lastModified.toInstant(), UTC))
         );
-        return buildMessage(uuid, publishReference, headers, msgBody);
+        return buildMessage(uuid, publishReference, headers, msgBody, contentType);
     }
 
-    private Message buildMessage(String uuid, String publishReference, Map<String,String> headers, MessageBody msgBody) {
+    private Message buildMessage(String uuid, String publishReference, Map<String, String> headers, MessageBody msgBody, String contentType) {
         Message msg;
         try {
+            String contentTypeMapping = contentTypeMappings.get(contentType);
+            if (contentTypeMapping == null) {
+                throw new MissingMappingForContentTypeException(contentType);
+            }
+
             String messageBody = objectMapper.writeValueAsString(msgBody);
             msg = new Message.Builder().withMessageId(UUID.randomUUID())
                     .withMessageType(CMS_CONTENT_PUBLISHED)
                     .withMessageTimestamp(new Date())
                     .withOriginSystemId(systemId)
-                    .withContentType("application/json")
+                    .withContentType(contentTypeMapping)
                     .withMessageBody(messageBody)
                     .build();
 
             msg.addCustomMessageHeader(TRANSACTION_ID_HEADER, publishReference);
-            for (Map.Entry<String,String> en : headers.entrySet()) {
+            for (Map.Entry<String, String> en : headers.entrySet()) {
                 msg.addCustomMessageHeader(en.getKey(), en.getValue());
             }
             msg = KeyedMessage.forMessageAndKey(msg, uuid);
@@ -89,8 +94,6 @@ public class MessageBuilder {
         public final String contentUri;
         @JsonProperty("lastModified")
         public final String lastModified;
-        @JsonProperty("contentType")
-        public final String contentType;
 
         MessageBody(
                 @JsonProperty("payload")
@@ -98,13 +101,10 @@ public class MessageBuilder {
                 @JsonProperty("contentUri")
                         String contentUri,
                 @JsonProperty("lastModified")
-                        String lastModified,
-                @JsonProperty("contentType")
-                        String contentType) {
+                        String lastModified) {
             this.contentUri = contentUri;
             this.payload = payload;
             this.lastModified = lastModified;
-            this.contentType = contentType;
         }
     }
 }
